6b8a83ab4817b3381d8cf72fa9826197f7a7cbda
Priala <132485581+Priala@users.noreply.github.com>
2023-08-09 21:15:01 +0200

    Implemented weather javascript and css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Princess Alao and is open-sourced on{" "}
-        <a
-          href="https://github.com/Priala/react-weather-app"
-          target="_blank"
-          rel="noreferrer">
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Princess Alao and is open-sourced on{" "}
+          <a
+            href="https://github.com/Priala/react-weather-app"
+            target="_blank"
+            rel="noreferrer">
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
