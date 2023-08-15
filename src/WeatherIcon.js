2094b4a2caee9321984453173ffcdc8d706cd171
Priala <132485581+Priala@users.noreply.github.com>
2023-08-15 19:38:26 +0200

    fixed icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactAnimatedWeather from "react-animated-weather/devDependencies/ReactAnimatedWeather";
+import ReactAnimatedWeather from "react-animated-weather/package.json";
 
 export default function WeatherIcon(props) {
   const codeMapping = {
