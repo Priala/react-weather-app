e67c404a12982c8704201d5af32765898a5f4f5a
Priala <132485581+Priala@users.noreply.github.com>
2023-08-15 19:05:17 +0200

    fixed apiurl

--- src/Weather.js
+++ src/Weather.js
@@ -21,11 +21,11 @@ export default function Weather(props) {
     });
   }
 
-  function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
+   function search() {
+     const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+     axios.get(apiUrl).then(handleResponse);
+   }
 
   function handleSubmit(event) {
     event.preventDefault();
