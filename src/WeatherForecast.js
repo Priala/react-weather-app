655d20d4774a305c6242e586c99bc534cd4b8385
Priala <132485581+Priala@users.noreply.github.com>
2023-08-27 15:25:27 +0200

    fixed apiUrl

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -40,7 +40,7 @@ export default function WeatherForecast(props) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}.7&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
     axios.get(apiUrl).then(handleResponse);
 
